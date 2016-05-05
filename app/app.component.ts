7530d44e38322d99c97447ba7201300c8970e60b
Amanda Stensland <amanda.stensland@publit.se>
2016-05-05 23:19:58 +0200

    Upgrades to Angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,12 @@
-import { Component } from 'angular2/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
-
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { DashboardComponent } from './dashboard.component';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: 'app/app.component.html',
-  styleUrls: ['app/app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService
-  ]
+  selector: 'app',
+  templateUrl: 'app.component.html'
 })
 
-@RouteConfig([
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  }
-])
-
 export class AppComponent {
-  title = 'Tour of Heroes';
+  constructor() {
+    console.log("Hello");
+  }
 }
--- app/main.ts
+++
@@ -1,3 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {HeroesComponent} from './heroes.component';
-bootstrap(HeroesComponent);
